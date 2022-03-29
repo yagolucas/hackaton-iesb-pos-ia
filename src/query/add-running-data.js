77adb26747fca080ee36867c6336277f4e0c08d0
Yago Lucas de Souza Freitas <cast.yago.utic@sebrae.com.br>
2022-03-29 18:23:20 -0300

    fazendo funcionar

---
+++ src/query/add-running-data.js
@@ -0,0 +1,66 @@
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://root:example@localhost:27017/";
+const path = require("path");
+const root = path.dirname(require.main.filename);
+const fs = require("fs");
+const util = require("util");
+const asyncs = require("async");
+
+const dataFolder = root + "/../../data/RUNNING/";
+
+const client = new MongoClient(url);
+const exec = async (colection) => {
+  try {
+    const data = [];
+    await client.connect();
+    const db = client.db("admin");
+    const collection = db.collection(colection);
+
+    fs.readdir(dataFolder, (err, files) => {
+      if (err) {
+        console.log("não conectei");
+        client.close();
+        return;
+      }
+      asyncs.mapLimit(
+        files,
+        10,
+        async (file) => {
+          const raw = await util.promisify(fs.readFile)(
+            dataFolder + file,
+            "utf-8"
+          );
+          const obj = JSON.parse(raw);
+          const utilData = obj.exercises[0].samples;
+          const filtered = {
+            rota: utilData.recordedRoute,
+            distancia: utilData.distance,
+            velocidade: utilData.speed,
+            cadence: utilData.cadence,
+          };
+          const doc = {
+            date: file.replace("_", ".").replace(".json", ""),
+            values: filtered,
+          };
+          data.push(doc);
+        },
+        async (err) => {
+          if (err) {
+            console.log(err);
+          }
+          if (data.length) {
+            await collection.insertMany(data);
+          }
+          client.close();
+        }
+      );
+    });
+    console.log("inserção");
+  } catch (e) {
+    console.log("não conectou!");
+    console.log(e.message);
+    client.close();
+  }
+};
+
+exec('RUNNING');
--- src/query/create-collection.js
+++ src/query/create-collection.js
@@ -1,19 +1,37 @@
 const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://root:example@localhost:27017/";
+const fs = require("fs");
+const path = require("path");
+const root = path.dirname(require.main.filename);
+const asyncs = require("async");
+
+const dataFolder = root + "/../../data/";
 
 const client = new MongoClient(url);
-const exec = async () => {
+const createCollections = async () => {
   try {
     await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    await client.db("admin").createCollection("hackaton");
-    console.log("criou colection");
+    fs.readdir(dataFolder, (err, files) => {
+      if (err) {
+        console.log("has error");
+        client.close();
+        return;
+      }
+      asyncs.mapLimit(
+        files,
+        10,
+        async (file) => await client.db("admin").createCollection(file),
+        () => client.close()
+      );
+    });
+    // await client.db("admin").createCollection("hackaton");
   } catch (e) {
     console.log("não conectou!");
     console.log(e.message);
-  } finally {
     client.close();
   }
 };
 
-exec();
+// listAvaliableCollections();
+// listAvaliableCollections2();
+createCollections();
